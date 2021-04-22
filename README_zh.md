a7a27b31d6983e2fc9dfcb9a5abcba8bc26c28a3
LAPTOP-32JQFO4L\h1mple <2292398086@qq.com>
2021-04-22 18:54:55 +0800

    [fix] fix array equal function

--- README_zh.md
+++ README_zh.md
@@ -3,4 +3,4 @@
 [![npm](https://img.shields.io/npm/v/hx_utils.svg)](https://www.npmjs.com/package/hx_utils)
 [![LICENSE MIT](https://img.shields.io/npm/l/hx_utils.svg)](https://www.npmjs.com/package/hx_utils)
 
-一个工具类的合集
+utils for web development
--- src/array/arrayEqual.js
+++ src/array/arrayEqual.js
@@ -1,14 +1,16 @@
 /**
- * 
- * @desc 判断两个数组是否相等
- * @param {Array} arr1 
- * @param {Array} arr2 
+ *
+ * @desc Determine whether two arrays are equal
+ * @param {Array} arr1
+ * @param {Array} arr2
  * @return {Boolean}
  */
 function arrayEqual(arr1, arr2) {
+    if (!arr1 || !arr2) return false;
     if (arr1 === arr2) return true;
-    if (arr1.length != arr2.length) return false;
-    for (var i = 0; i < arr1.length; ++i) {
+    if (arr1.length !== arr2.length) return false;
+    var len = arr1.length;
+    for (var i = 0; i < len; ++i) {
         if (arr1[i] !== arr2[i]) return false;
     }
     return true;
